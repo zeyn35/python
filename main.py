e4ac45fc1e0f64f452d33bb4273eb702128675f1
Vlad <novosadvlad269@gmail.com>
2026-09-12 13:54:28 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+print("Hello world")
+
+x = 10
+name = "Dima"
+
+user_name = "Igor"
+total_price = 100
+
+NUMBER_PI = 3.14
+
+
+print(user_name)
+print("Имя: ", user_name)
+
+print('Python', 'Java', 'C++', sep=",",end=".")
+
+print("Hello", end="!")
+print("World")
+
+number_a = 10
+number_b = 3
+
+print(number_a + number_b)
+print(number_a - number_b)
+print(number_a * number_b)
+print(number_a / number_b)
+
+print(number_a // number_b)
+print(number_a % number_b)
+print(number_a ** number_b)
+
+my_name = input("Как вас зовут?   ")
+print(my_name)
